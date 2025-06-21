291a5c6bae92ada685f5d2677cc7a96c6eaf90b8
Rajat Singh <rs738489@gmail.com>
2025-06-21 10:22:51 +0530

    User home ui completed

---
+++ frontend/src/components/ChooseVeichleOption.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+const ChooseVeichleOption = (props) => {
+  return (
+    <div>
+      <h2 className="mb-5 font-semibold text-2xl">Choose your ride</h2>
+      <div
+        onClick={() => {
+          props.setConfirmVeichle(true),
+            props.setveichleSelection(false),
+            props.setPannelOpen(false);
+        }}
+        className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between "
+      >
+        <img
+          className="h-10 mb-6"
+          src="https://www.pngplay.com/wp-content/uploads/8/Uber-PNG-Photos.png"
+          alt=""
+        />
+        <div className=" w-1/2">
+          <h4 className="font-medium text-sm">
+            FastDriveGo{" "}
+            <span>
+              <i className="ri-user-fill"></i>4
+            </span>
+          </h4>
+          <h5 className="font-medium text-sm">2 mins away</h5>
+          <p className="font-medium text-xs ">Affordable, compact rides</p>
+        </div>
+        <h2 className="text-xl font-semibold pb-9">rs200.00</h2>
+      </div>
+      <div
+        onClick={() => {
+          props.setConfirmVeichle(true),
+            props.setveichleSelection(false),
+            props.setPannelOpen(false);
+        }}
+        className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between "
+      >
+        <img
+          className="h-10 mb-6"
+          src="https://static.vecteezy.com/system/resources/previews/028/051/292/original/electric-motorbike-electric-bike-electric-vehicle-transparent-background-ai-generated-png.png"
+          alt=""
+        />
+        <div className=" w-1/2">
+          <h4 className="font-medium text-sm">
+            FastDriveGo{" "}
+            <span>
+              <i className="ri-user-fill"></i>1
+            </span>
+          </h4>
+          <h5 className="font-medium text-sm">2 mins away</h5>
+          <p className="font-medium text-xs ">Affordable, compact rides</p>
+        </div>
+        <h2 className="text-xl font-semibold pb-9">rs200.00</h2>
+      </div>
+      <div
+        onClick={() => {
+          props.setConfirmVeichle(true),
+            props.setveichleSelection(false),
+            props.setPannelOpen(false);
+        }}
+        className="flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between "
+      >
+        <img
+          className="h-10 mb-6"
+          src="https://clipart-library.com/2023/Uber_Auto_312x208_pixels_Mobile.png"
+          alt=""
+        />
+        <div className=" w-1/2">
+          <h4 className="font-medium text-sm">
+            FastDriveGo{" "}
+            <span>
+              <i className="ri-user-fill"></i>3
+            </span>
+          </h4>
+          <h5 className="font-medium text-sm">2 mins away</h5>
+          <p className="font-medium text-xs ">Affordable, compact rides</p>
+        </div>
+        <h2 className="text-xl font-semibold pb-9">rs200.00</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ChooseVeichleOption;
---
+++ frontend/src/components/ConfirmVeichle.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const ConfirmVeichle = (props) => {
+  return (
+    <div>
+      <h5
+        className="p-1 text-center w-[93%] absolute top-0"
+        onClick={() => {
+          props.setConfirmVeichle(false);
+
+        }}
+      >
+        <i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i>
+      </h5>
+      <h3 className="text-2xl font-semibold mb-5">Confirm Your Ride</h3>
+
+      <div className="flex gap-2 justify-between flex-col items-center">
+        <img
+          src="https://th.bing.com/th/id/OIP.ymjpxr4RPlwbLenCbbpYywHaE7?rs=1&pid=ImgDetMain"
+          alt="veichle"
+          className="h-20"
+        />
+        <div className="w-full mt-5">
+          <div className="flex items-center gap-5 border-b-2">
+            <i className="ri-map-pin-line"></i>
+            <div>
+              <h3 className="text-lg font-medium">562/11-A</h3>
+              <p className="text-sm -mt-1 text-gray-600">
+                Kirloskar Laoyout, Bangalore, 260090
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-5 p-3 border-b-2">
+            <i className="ri-map-pin-line"></i>
+            <div>
+              <h3 className="text-lg font-medium">562/11-A</h3>
+              <p className="text-sm -mt-1 text-gray-600">Kirloskar Layout</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-5 p-3">
+            <i className="ri-currency-line"></i>
+            <div>
+              <h3 className="text-lg font-medium">Rs200</h3>
+              <p className="text-sm -mt-1 text-gray-600">Cash</p>
+            </div>
+          </div>
+        </div>
+        <button 
+        onClick={()=>{
+          props.setConfirmVeichle(false)
+          props.setveichleFound(true)
+        }}
+        className="w-full bg-green-600 font-semibold p-2 rounded-lg">
+          Confirm
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmVeichle;
