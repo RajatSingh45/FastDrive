ed6d212447d8d855183d21ccc3125cf75aef0513
Rajat Singh <rs738489@gmail.com>
2025-10-19 13:35:53 +0530

    Tried to resolve CaptainLiveTracking import issue

--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -5,6 +5,7 @@ import FinishRide from "../components/FinishRide";
 import gsap from "gsap";
 import CaptainLiveTracking from "../components/CaptainLiveTracking";
 
+
 const CaptainRiding = () => {
   const [finishRidePannel, setFinishRidePannel] = useState(false);
   const finishRidePannelRef = useRef(null);
