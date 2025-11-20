8ea099e515eb254a05ce7a835f9929789b862e00
Rajat Singh <rs738489@gmail.com>
2025-11-20 15:34:20 +0530

    fixed marker issue

--- frontend/src/components/HomeMap.jsx
+++ frontend/src/components/HomeMap.jsx
@@ -9,6 +9,16 @@ import {
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+});
+
 // This component handles finding and marking the current location
 function LocationMarker({ onLocationFound }) {
   const [position, setPosition] = useState(null);
